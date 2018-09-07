4ac5932b296c5af44f8b971fef494c358d67926b
anastafj197 <frankie.anastasia@gmail.com>
2018-09-07 19:00:33 -0400

    Added HostListener
    To attach event to directive
    Change color when hovering and leaving

--- highlightMe.directive.ts
+++ highlightMe.directive.ts
@@ -1,11 +1,23 @@
-import {Directive, ElementRef} from 'angular2/core';
+import {Directive, ElementRef, HostListener} from 'angular2/core';
 
 @Directive({
 	selector: '[highlightMe]'
 })
 
 export class HighlightMeDirective {
-	constructor(el: ElementRef) {
-		el.nativeElement.style.backgroundColor = 'Green';
+	constructor(private el: ElementRef) {
+		this.el.nativeElement.style.backgroundColor = 'Green';
+	}
+
+	@HostListener ('mouseenter') onMouseEnter(){
+		this.highlight('yellow');
+	}
+
+	@HostListener ('mouseleave') onMouseLeave(){
+		this.highlight('blue');
+	}
+
+	private highlight(color : string) {
+		this.el.nativeElement.style.backgroundColor = color;
 	}
 }
